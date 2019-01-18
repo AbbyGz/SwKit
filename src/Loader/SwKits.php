fc720a35d907c77f519e29594252e8f123190892
DarkByx <atanacio398@gmail.com>
2019-01-18 17:56:58 -0500

    create main loader

---
+++ src/Loader/SwKits.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Loader;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class SwKits extends PluginBase implements Listener {
+
+}
